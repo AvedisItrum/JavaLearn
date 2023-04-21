3ec0694046ad1250a6aa73efcb7c03dd435a6097
AvedisItrum <avedis.itrum@gmail.com>
2023-04-21 13:35:03 +0300

    Compare ArrayList And LinkedList

--- src/ListTest.java
+++ src/ListTest.java
@@ -12,14 +12,14 @@ public class ListTest {
         for (int i = 0; i < 5; i++) {
             ArrayList<Integer> AL = new ArrayList<>();
             startTime = System.currentTimeMillis();
-            for (Integer A = 0; A < 10_000_000; A++) {
+            for (Integer A = 0; A < 50_000_000; A++) {
                 AL.add(i * A);
             }
             ALTimes.add((int) (System.currentTimeMillis() - startTime));
 
             LinkedList<Integer> LL = new LinkedList<>();
             startTime = System.currentTimeMillis();
-            for (Integer L = 0; L < 10_000_000; L++) {
+            for (Integer L = 0; L < 50_000_000; L++) {
                 LL.add(i * L);
             }
             LLTimes.add((int) (System.currentTimeMillis() - startTime));
@@ -33,10 +33,16 @@ public class ListTest {
         System.out.println((float) AL_AVG / (float) LL_AVG * 100 + "%");
 
         /*
-        Для 10 млн добавленией
+        Для 10 млн добавлений
         ArrayList AVG = 551 млс в среднем
         LinkedList AVG = 1188 млс в среднем
         Разница 46.38047%
+
+        Для 50 млн добавлений
+        ArrayList AVG = 2342
+        LinkedList AVG = 6917
+        33.85861%
+
 */
     }
 }
