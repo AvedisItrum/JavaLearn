ff725093de64a53e9dd5361d6b50a3c417fe2392
AvedisItrum <avedis.itrum@gmail.com>
2023-04-27 18:40:58 +0300

    Consecutive strings

--- src/CodeWars.java
+++ src/CodeWars.java
@@ -728,21 +728,6 @@ public class CodeWars {
             str.append(strarr[g + j]);
         return str.toString();
     }
-    private static void testing(String actual, String expected) {
-        assertEquals(expected, actual);
-    }
-    @Test
-    public void test() {
-        System.out.println("longestConsec Fixed Tests");
-        testing(longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2), "abigailtheta");
-        testing(longestConsec(new String[] {"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, 1), "oocccffuucccjjjkkkjyyyeehh");
-        testing(longestConsec(new String[] {}, 3), "");
-        testing(longestConsec(new String[] {"itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"}, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
-        testing(longestConsec(new String[] {"wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"}, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
-        testing(longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas"}, -2), "");
-        testing(longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 3), "ixoyx3452zzzzzzzzzzzz");
-        testing(longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 15), "");
-        testing(longestConsec(new String[] {"it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"}, 0), "");
-    }
-    //66/74
+
+    //67/74
 }
